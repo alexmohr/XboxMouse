9b7992700e3b19c0b3a8cccf6408b432792ff1e6
alexmohr <alex.mohr.mail@gmail.com>
2014-09-23 23:30:52 +0200

    Added more base stuff ( no code yet )

---
+++ Development/Source/XBoxMouse/ControllerAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerAccess
+{
+    public class Class1
+    {
+    }
+}
